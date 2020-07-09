13bbae56b34fd39546c70ab95f86444792a69ffb
Kevin Apetrei <65184258+RealKevinApetrei@users.noreply.github.com>
2020-07-10 00:57:48 +0100

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-# GAME CONFIG # ONLY EDIT IF NEEDED #
-decrease_rate = 5 # Range Decrease Rate per round (Default: 5)
-start_score = 0 # Starting Score (Default: 0)
-num_range = 100 # Starting Number Range (Default: 100)
-# GAME CONFIG # ONLY EDIT IF NEEDED #
